ebadf7055800c89fa60122cc9a17e7a5ccdbe0c3
dullat <142017979+Dullat@users.noreply.github.com>
2025-09-30 19:49:28 +0530

    use of advance query

--- advanceQuery/src/app/store.js
+++ advanceQuery/src/app/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
-import postsReducer from '../features/posts/postsSlice';
-import usersReducer from '../features/users/usersSlice';
-
+import { apiSlice } from "../features/api/apiSlice";
+import usersReducer from "../features/users/usersSlice";
 
 export const store = configureStore({
-    reducer: {
-        posts: postsReducer,
-        users: usersReducer
-    }
-})
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    users: usersReducer,
+  },
+  // required to use query
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware), // manage lifetime, cache etc
+});
+
